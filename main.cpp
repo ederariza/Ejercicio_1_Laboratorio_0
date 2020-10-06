73bc09e8539365e89b578273dd3400dfe594f7bf
Infromatica II <eder.ariza@udea.edu.co>
2020-10-05 22:56:46 -0500

    Ejercicio 1 Laboratorio 0

--- main.cpp
+++ main.cpp
@@ -15,31 +15,37 @@ A B C D E F G F E D C B A
 
 #include<iostream>
 using namespace std;
-int  i, j, k, N;
 
+//Declarando las variables que necesito para las iteraciones
+int  i, j, k, N;
 char letra;
+
 int main(void){
 
-    cout<< "Ingrese el numero de dimension del triangulo mariposa: ";
-    cin>> N;
+    cout << "Ingrese el numero de dimension del triangulo mariposa: ";
+    cin >> N;
 
-    for(i=1; i<=N; i++){
+    for (i = 1; i <= N; i++){
         letra = 65;
 
-        for(j=1; j<=i; j++){
-            cout<< letra <<" ";
+        //Imprime letras del abecedario en orden + 1 espacio:  A + " " + B + ...
+        for (j = 1; j <= i; j++){
+            cout << letra << " ";
             letra++;
         }
 
-        for(j=0; j<((N-i)*2-1); j++){
-            cout<<"  ";
+        //Imprime 2 espacios "  "...
+        for (j = 0; j < ((N-i)*2-1); j++){
+            cout << "  ";
         }
 
-        for(j=1; j<=i; j++){
+        //Imprime las letras del abecedario en orden inverso + 1 espacio: ... + B + " " + A
+        for (j = 1; j <= i; j++){
             letra--;
 
-            if(letra != (65+N-1)){
-                cout<<letra << " ";
+            //Omitimos la ultima letra N + 65
+            if (letra != (65+N-1)){
+                cout << letra << " ";
             }
         }
 
